587f706037b1409845f45bdcdc4a52f3455e25c3
YeongSeon Yun <dudtjswnstjscoding@gmail.com>
2024-04-13 15:31:41 +0900

    2024/4/12

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './style.css'; // 스타일 시트를 가져옵니다.
 
-function App() {
+function TimeText() {
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+
+  useEffect(() => {
+      const update = () => {
+          const today = new Date();
+          setSeconds(today.getSeconds());
+          setMinutes(today.getMinutes());
+          setHours(today.getHours());
+      }
+
+      update();
+
+      const timeFlow = setInterval(() => {
+          update();
+      }, 1000);
+
+      return () => clearInterval(timeFlow);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="timeStyle">
+      {hours.toString().padStart(2, "0")} :{" "}
+      {minutes.toString().padStart(2, "0")} :{" "}
+      {seconds.toString().padStart(2, "0")}
     </div>
   );
 }
 
+function ToDoItem({ text, onDeleteClicked }) {
+  return (
+    <div className="toDoItemStyle">
+      <label>
+        <input className="checkboxStyle" type="checkbox" defaultChecked={false} />
+        {text}
+      </label>
+      <button className="deleteButtonStyle" onClick={onDeleteClicked}></button>
+    </div>
+  );
+}
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDosList, setToDosList] = useState([]);
+
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(toDo);
+
+    setToDosList((prevArray) => [...prevArray, toDo]);
+    setToDo("");
+  };
+
+  const onDeleteToDo = (index) => {
+    const currentArray = [...toDosList];
+    currentArray.splice(index, 1);
+    
+    setToDosList(currentArray);
+  };
+
+  return (
+    <div>
+      <TimeText /> {/* 시간 텍스트 컴포넌트 추가 */} 
+      <form onSubmit={onSubmit}>
+        <input
+          className="toDoInputStyle"
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+      </form>
+      <hr />
+      <ul>
+        {toDosList.map((item, index) => (
+          <ToDoItem key={index} text={item} onDeleteClicked={() => onDeleteToDo(index)} />
+        ))}
+      </ul>
+    </div>
+  );
+}
 export default App;
+
+
+
+
+
+  
